c3dd9e664d88721cf407274ed9800b8433ce3fc0
Akanksha Priya <akankshapriya2162000@gmail.com>
2020-08-18 13:43:07 +0530

    added new dbname

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 // const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
-const dbname = 'conFusion';
+const dbname = 'language';
 
 
 /* MongoClient.connect(url, (err, client) => {
@@ -48,8 +48,8 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
+    const collection = db.collection("webd");
+    collection.insertOne({"name": "Uthappizza", "description": "test", "level": "easy"},
     (err, result) => {
         assert.equal(err,null);
 
@@ -62,7 +62,7 @@ MongoClient.connect(url, (err, client) => {
             console.log("Found:\n");
             console.log(docs);
 
-            db.dropCollection("dishes", (err, result) => {
+            db.dropCollection("webd", (err, result) => {
                 assert.equal(err,null);
 
                 client.close();
