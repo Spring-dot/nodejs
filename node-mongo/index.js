4ccfe6168f023049780aca5a1566396c44cfbbe3
Akanksha Priya <akankshapriya2162000@gmail.com>
2020-08-18 21:42:25 +0530

    updated operations js with old code

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,77 +1,41 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-// const dboper = require('./operations');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
-const dbname = 'language';
+const dbname = 'conFusion';
 
 
-/* MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url, (err, client) => {
     assert.equal(err, null);
 
     console.log('Server connected successfully');
 
     const db = client.db(language);
-    const collection = db.collection("webd");
+    const collection = db.collection("dishes");
+ 
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+    "dishes", (result) => {
+        console.log("Insert Document:\n", result.ops);
 
-    collection.insertOne({"name": "html", "description": "basic lang", "level": "easy"},
-                           // {"name": "python", "description": "basic other lang", "level": "medium"},
-     (err, result) => {
-        assert.equal(err, null);
-        console.log("After insert: \n");
-        console.log(result.ops);
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
+            dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes",
+                (result) => {
+                    console.log("Updated Document:\n", result.result);
 
-            db.dropCollection("webd", (err, result) => {
-                assert.equal(err,null);
+                    dboper.findDocuments(db, "dishes", (docs) => {
+                        console.log("Found Updated Documents:\n", docs);
+                        
+                        db.dropCollection("dishes", (result) => {
+                            console.log("Dropped Collection: ", result);
 
-                client.close();
-            });
-        });
-
-
-       
-    });
-
-
-}); */
-
-MongoClient.connect(url, (err, client) => {
-
-    assert.equal(err,null);
-
-    console.log('server is not connected');
-
-    const db = client.db(dbname);
-    const collection = db.collection("webd");
-   // var langs = [{"name": "Uthappizza", "description": "test", "level": "easy"},
-     //            {"name": "python", "description": "other lang", "level": "easy"}];
-    collection.insertMany([{"name": "Uthappizza", "description": "test", "level": "easy"},
-    {"name": "python", "description": "other lang", "level": "easy"}],
-    (err, result) => {
-        assert.equal(err,null);
-
-        console.log("After Insert:\n");
-        console.log(result.ops);
-
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
-
-            db.dropCollection("webd", (err, result) => {
-                assert.equal(err,null);
-
-                client.close();
+                            client.close();
+                        });
+                    });
             });
         });
     });
-
 });
-
